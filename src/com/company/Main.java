b8eb0d49c1f102a0cfb0dbc4691436219f7bb1b3
PavloKobelezky <102289447+PavloKobelezky@users.noreply.github.com>
2022-03-24 12:13:25 +0200

    Add files via upload

---
+++ src/com/company/Main.java
@@ -0,0 +1,31 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        //Завдання 1
+        int a = 25;
+        int b = 5;
+        int c = a / b;
+        int d = a % b;
+
+        System.out.println("Завдання 1: "+c+"; "+d);
+
+        //Завдання 2
+        int f = 92;
+        int h = 92 % 10;
+        int x = (f/10)%10;
+
+        System.out.println("Завдання 2: "+(f+h));
+
+        //Завдання 3
+        double k = 3.4;
+        double m = Math.round(k);
+
+        System.out.println("Завдання 3: "+m);
+
+    }
+
+
+
+}
